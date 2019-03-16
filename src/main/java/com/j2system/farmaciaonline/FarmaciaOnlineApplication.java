d66cfe90484df89f887bca16448b8a7a37867f50
VictorJunior <victorcastrojunior@yahoo.com.br>
2019-03-16 09:51:59 -0300

    Projeto criado

---
+++ src/main/java/com/j2system/farmaciaonline/FarmaciaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.j2system.farmaciaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaOnlineApplication.class, args);
+	}
+
+}
